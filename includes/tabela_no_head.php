bc443716dc6eadd46321ce31b4dfbab021525992
elfiservice <elfiservice@hotmail.com>
2016-02-24 17:27:36 -0300

    feita pasta includes

---
+++ includes/tabela_no_head.php
@@ -0,0 +1,19 @@
+<!-- Tabela  -->
+<link rel="stylesheet" href="../tabela/demo_page.css">  
+<link rel="stylesheet" href="../tabela/demo_table.css">  
+
+		<script type="text/javascript" language="javascript" src="../tabela/jquery.js"></script>
+		<script type="text/javascript" language="javascript" src="../tabela/jquery.dataTables.js"></script>
+		<script type="text/javascript" charset="utf-8">
+			$(document).ready(function() {
+				$('#example').dataTable();
+			} );
+			
+						$(document).ready(function() {
+				$('#example2').dataTable();
+			} );
+
+						$(document).ready(function() {
+				$('#example3').dataTable();
+			} );			
+		</script>
