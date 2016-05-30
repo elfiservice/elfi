093dd88c5193cff7b4a840999f15277d02731f04
elfiservice <elfiservice@hotmail.com>
2016-05-30 13:35:47 -0300

    alterado o Config.inc e adicionado Classes PHPMailer e iniciado alteracao EmailModel

--- classes/Config.inc.php
+++ classes/Config.inc.php
@@ -1,35 +1,82 @@
 <?php
 //require '../Config/SistemConfig.php';
-$tree = $_SERVER['DOCUMENT_ROOT'].'/site ELFI/colaboradores/';
+//$tree = $_SERVER['DOCUMENT_ROOT'].'/site ELFI/colaboradores/';
 //$tree = $_SERVER['DOCUMENT_ROOT'].'/colaboradores/'; //servidor
 //echo $tree;
 
 //require $tree.'teste/teste.php';
-require $tree.'classes/controller/ClienteCtrl.class.php';
-require $tree.'classes/controller/OrcamentosCtrl.class.php';
-require $tree.'classes/controller/UsuarioCtrl.class.php';
-require $tree.'classes/controller/ColaboradorCtrl.class.php';
-require $tree.'classes/controller/EmpresaCtrl.class.php';
+//require $tree.'classes/controller/ClienteCtrl.class.php';
+//require $tree.'classes/controller/OrcamentosCtrl.class.php';
+//require $tree.'classes/controller/UsuarioCtrl.class.php';
+//require $tree.'classes/controller/ColaboradorCtrl.class.php';
+//require $tree.'classes/controller/EmpresaCtrl.class.php';
+//
+//require $tree.'classes/dao/ClienteDAO.class.php';
+//require $tree.'classes/dao/OrcamentoDAO.class.php';
+//require $tree.'classes/dao/UsuarioDAO.class.php';
+//require $tree.'classes/dao/ColaboradorDAO.class.php';
+//require $tree.'classes/dao/EmpresaDAO.class.php';
+//
+////require $tree.'classes/model/Cliente.class.php';
+////require $tree.'classes/model/Cliente_PF.class.php';
+////require $tree.'classes/model/Cliente_PJ.class.php';
+////require $tree.'classes/model/EmailModel.class.php';
+////require $tree.'classes/model/EmailOrcamentoNaoAprovado.class.php';
+////require $tree.'classes/model/Orcamento.class.php';
+////require $tree.'classes/model/Usuario.class.php';
+////require $tree.'classes/model/Colaborador.class.php';
+////require $tree.'classes/model/Empresa.class.php';
+//
+//require $tree.'classes/util/Conexao.class.php';
+//require $tree.'classes/util/Formatar.class.php';
+//require $tree.'classes/util/Read.class.php';
+//require $tree.'classes/util/Update.class.php';
+//require $tree.'classes/util/Login.class.php';
+//require $tree.'classes/util/Insert.class.php';
 
-require $tree.'classes/dao/ClienteDAO.class.php';
-require $tree.'classes/dao/OrcamentoDAO.class.php';
-require $tree.'classes/dao/UsuarioDAO.class.php';
-require $tree.'classes/dao/ColaboradorDAO.class.php';
-require $tree.'classes/dao/EmpresaDAO.class.php';
+spl_autoload_register(function ($pClass) {
+    $tree = $_SERVER['DOCUMENT_ROOT'].'/site ELFI/colaboradores/classes';
+    
+      if(file_exists("{$tree}/model/{$pClass}.class.php")){
+        //echo "tem Arquivo!";
+        require_once "{$tree}/model/{$pClass}.class.php";
+        
+        }else if(file_exists("{$tree}/controller/{$pClass}.class.php")){
+        //echo "tem Arquivo!";
+        require_once "{$tree}/controller/{$pClass}.class.php";
+        
+        }else if(file_exists("{$tree}/dao/{$pClass}.class.php")){
+        //echo "tem Arquivo!";
+        require_once "{$tree}/dao/{$pClass}.class.php";
+        
+        }else if(file_exists("{$tree}/util/{$pClass}.class.php")){
+        //echo "tem Arquivo!";
+        require_once "{$tree}/util/{$pClass}.class.php";
+        
+        } else {
+            die ("Erro ao incluir {$pClass}.class.php<hr>");
+        }
 
-require $tree.'classes/model/Cliente.class.php';
-require $tree.'classes/model/Cliente_PF.class.php';
-require $tree.'classes/model/Cliente_PJ.class.php';
-require $tree.'classes/model/EmailModel.class.php';
-require $tree.'classes/model/EmailOrcamentoNaoAprovado.class.php';
-require $tree.'classes/model/Orcamento.class.php';
-require $tree.'classes/model/Usuario.class.php';
-require $tree.'classes/model/Colaborador.class.php';
-require $tree.'classes/model/Empresa.class.php';
 
-require $tree.'classes/util/Conexao.class.php';
-require $tree.'classes/util/Formatar.class.php';
-require $tree.'classes/util/Read.class.php';
-require $tree.'classes/util/Update.class.php';
-require $tree.'classes/util/Login.class.php';
-require $tree.'classes/util/Insert.class.php';
+//    $dirName = array('model', 'controller', 'dao', 'util');
+//   
+//    foreach ($dirName as $pasta){
+//        
+//        $pastaFinal = $tree.$pasta;
+//        
+//        if(file_exists("{$pastaFinal}/{$pClass}.class.php")){
+//        //echo "tem Arquivo!";
+//        require_once "{$pastaFinal}/{$pClass}.class.php";
+//        
+//        }else{
+//           // die ("Erro ao incluir {$pastaFinal}/{$pClass}.class.php<hr>");
+//        }
+        
+ //   }
+    
+
+
+
+});
+
+
--- classes/model/EmailModel.class.php
+++ classes/model/EmailModel.class.php
@@ -1,17 +1,43 @@
 <?php
-
+require '../util/PHPMailerAutoload.php/';
 abstract class EmailModel{
 	
 	private $email;
 	private $image_name = "http://elfiservice.eco.br/colaboradores/includes/email/logo_elfi_email.jpg";
 	private $assunto_email;
+        /**
+         *
+         * @var PHPMailer
+         */    
+        private $phpMailer;
 
-	public function enviarEmail(){
-		
-	}
+        
+        public function configEmailSmtp() {
+//            $this->phpMailer = new PHPMailer();
+//            $this->phpMailer->CharSet = "UTF-8";
+            $this->setPhpMailer(new PHPMailer);
+            $this->getPhpMailer()->CharSet = "UTF-8";
+            $this->getPhpMailer()->IsSMTP();
+            $this->getPhpMailer()->Host = 'smtp.elfiservice.com.br';
+            $this->getPhpMailer()->SMTPAuth = true;
+            $this->getPhpMailer()->Username = 'elfi@elfiservice.com.br';
+            $this->getPhpMailer()->Password = 'Sapato44';
+            $this->getPhpMailer()->Port = 587;
+            $this->getPhpMailer()->AddAttachment($this->getImageName());
+            $this->getPhpMailer()->IsHTML(true);
+                           
+            
+        }
 	
+        public function getPhpMailer() {
+            return $this->phpMailer;
+        }
 
-	public function getEmail() {
+        public function setPhpMailer(PHPMailer $phpMailer) {
+            $this->phpMailer = $phpMailer;
+        }
+
+        	public function getEmail() {
 	         return $this->email;
 	}
 	
@@ -40,6 +66,3 @@ abstract class EmailModel{
 	
 }
 
-
-
-?>
