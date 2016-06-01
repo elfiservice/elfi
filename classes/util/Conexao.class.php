d4c4ce51da6497e37cbcca9eb00066b39c06d8f9
elfiservice <elfiservice@hotmail.com>
2016-06-01 10:38:18 -0300

    Alterado Conexao.class para mysqli

--- classes/util/Conexao.class.php
+++ classes/util/Conexao.class.php
@@ -18,19 +18,19 @@ class  Conexao {
 	
 	
 	public function conectar(){
-		$conn = @mysql_connect("localhost","root","") or die ("O servidor não responde!");
+		//$conn = @mysql_connect("localhost","root","") or die ("O servidor nao responde!");
 		
 		//conecta-se ao banco de dados
-		$db = mysql_select_db("elfiserv_sistema_elfi",$conn)
-		or die ("Não foi possivel conectar-se ao banco de dados!");
-		//new mysqli('localhost','root','','elfiserv_sistema_elfi');
+		//$db = @mysql_select_db("elfiserv_sistema_elfi",$conn)
+		//or die ("Nao foi possivel conectar-se ao banco de dados!");
+		new mysqli('localhost','root','','elfiserv_sistema_elfi');
 		//$mysqli = new mysqli($this->servidor,$this->usuario_servidor,'','elfiserv_sistema_elfi');
 		// = $mysqli->query('SELECT * FROM colaboradores');
 		//$query->num_rows;
 	}
 	
 	public static function desconectar(){
-		mysql_close(mysql_connect($this->servidor,$this->usuario_servidor,$this->senha_servidor)) or die ("Não foi possivel DESconectar-se ao banco de dados!");;
+		mysql_close(mysql_connect($this->servidor,$this->usuario_servidor,$this->senha_servidor)) or die ("Nï¿½o foi possivel DESconectar-se ao banco de dados!");;
 	}
 
 	
