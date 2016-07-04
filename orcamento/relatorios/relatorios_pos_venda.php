2bafda6efddfee6fce0fd740999a8ca62d4fa3f4
elfiservice <elfiservice@hotmail.com>
2016-07-04 17:47:32 -0300

    indentado cod relatorio pos venda

--- orcamento/relatorios/relatorios_pos_venda.php
+++ orcamento/relatorios/relatorios_pos_venda.php
@@ -23,7 +23,7 @@ $orcCrtl = new OrcamentoCtrl();
     <legend>Busca por ano</legend>
     <!-- form Trocar ANO -->
     <div><!-- form Trocar ANO -->
-        <form action="tecnico.php?id_menu=relatorios_cliente" method="post" enctype="multipart/form-data" name="formAgenda">
+        <form action="tecnico.php?id_menu=relatorios_pos_venda" method="post" enctype="multipart/form-data" name="formAgenda">
             Selecione o ANO:	
             <select name="ano" id="ano" class="formFieldsAno">
                 <option value="<?php echo $ano_orc_selec; ?>"><?php echo $ano_orc_selec; ?></option>
@@ -48,7 +48,7 @@ $orcCrtl = new OrcamentoCtrl();
 
     <?php
     $pesquisaCtrl = new PesquisaPosVendaCtrl();
-    $pesquisasRespondidas = $pesquisaCtrl->buscarControleNOrc("*", "");
+    $pesquisasRespondidas = $pesquisaCtrl->buscarControleNOrc("*", "WHERE YEAR(data) = '$ano_orc_selec' ");
     $totalPesquisasRespondidas = count($pesquisasRespondidas);
 
     $orcConcluidos = $orcCrtl->buscarOrcamentos("*", "WHERE YEAR(data_adicionado_orc) = '$ano_orc_selec' AND serv_concluido = 's' ");
@@ -59,7 +59,7 @@ $orcCrtl = new OrcamentoCtrl();
         <ul class="w3-ul w3-border w3-col l4 m5">
             <li><span class="w3-badge elfi_cor_fundo_tabela elfi_cor_padrao_texto w3-right"><?= $totalOrcConcluidos ?></span> Orçamentos Concluidos</li>
             <li ><span class="w3-badge elfi_cor_fundo_tabela elfi_cor_padrao_texto  w3-right"><?= $totalPesquisasRespondidas ?></span> Pesquisas Respondidas</li> 
-            <li class="elfi_cor_fundo_indice elfi_cor_padrao_texto"><span class="w3-badge elfi_cor_fundo_branco elfi_cor_padrao_texto w3-right"><?= Formatar::moedaBR(($totalPesquisasRespondidas / $totalOrcConcluidos) * 100) . "%" ?></span> Total</li> 
+            <li class="elfi_cor_fundo_indice elfi_cor_padrao_texto"><span class="w3-badge elfi_cor_fundo_branco elfi_cor_padrao_texto w3-right"><?= Formatar::moedaBR(($totalPesquisasRespondidas / ($totalOrcConcluidos == 0 ? 1 : $totalOrcConcluidos)) * 100) . "%" ?></span> Total</li> 
         </ul>
     </div>
 
@@ -71,41 +71,39 @@ $orcCrtl = new OrcamentoCtrl();
 
     <?php
     //$totalPesquisasRespondidas = count($pesquisasRespondidas);
- $nDeConfiabilidade = $pesquisaCtrl->buscarControleNOrc("*", "WHERE confiabilidade = '1' AND YEAR(data) = '$ano_orc_selec' ");
- $nDepontualidade = $pesquisaCtrl->buscarControleNOrc("*", "WHERE pontualidade = '1'  AND YEAR(data) = '$ano_orc_selec'  ");
- $nDedisponibilide = $pesquisaCtrl->buscarControleNOrc("*", "WHERE disponibilide = '1'  AND YEAR(data) = '$ano_orc_selec'  ");
- $nDequalidade = $pesquisaCtrl->buscarControleNOrc("*", "WHERE qualidade = '1'  AND YEAR(data) = '$ano_orc_selec'  ");
- $nDenormasseguranca = $pesquisaCtrl->buscarControleNOrc("*", "WHERE normasseguranca = '1'  AND YEAR(data) = '$ano_orc_selec'  ");
- $nDeapresentacao = $pesquisaCtrl->buscarControleNOrc("*", "WHERE apresentacao = '1'  AND YEAR(data) = '$ano_orc_selec'  ");
- $nDeenvolvimento = $pesquisaCtrl->buscarControleNOrc("*", "WHERE envolvimento = '1'  AND YEAR(data) = '$ano_orc_selec'  ");
- $nDeeducacao = $pesquisaCtrl->buscarControleNOrc("*", "WHERE educacao = '1'  AND YEAR(data) = '$ano_orc_selec'  ");
- $nDeorganizacao = $pesquisaCtrl->buscarControleNOrc("*", "WHERE organizacao = '1'  AND YEAR(data) = '$ano_orc_selec'  ");
- $nDecompetencia = $pesquisaCtrl->buscarControleNOrc("*", "WHERE competencia = '1'  AND YEAR(data) = '$ano_orc_selec'  ");
- $nDeorcamento= $pesquisaCtrl->buscarControleNOrc("*", "WHERE orcamento = '1'  AND YEAR(data) = '$ano_orc_selec'  ");
- $nDeservico= $pesquisaCtrl->buscarControleNOrc("*", "WHERE servico = '1'  AND YEAR(data) = '$ano_orc_selec'  ");
- $nDesatisfeito = $pesquisaCtrl->buscarControleNOrc("*", "WHERE satisfeito = '1'  AND YEAR(data) = '$ano_orc_selec'  ");
- //var_dump($nDeConfiabilidade);
-
-    
+    $nDeConfiabilidade = $pesquisaCtrl->buscarControleNOrc("*", "WHERE confiabilidade = '1' AND YEAR(data) = '$ano_orc_selec' ");
+    $nDepontualidade = $pesquisaCtrl->buscarControleNOrc("*", "WHERE pontualidade = '1'  AND YEAR(data) = '$ano_orc_selec'  ");
+    $nDedisponibilide = $pesquisaCtrl->buscarControleNOrc("*", "WHERE disponibilide = '1'  AND YEAR(data) = '$ano_orc_selec'  ");
+    $nDequalidade = $pesquisaCtrl->buscarControleNOrc("*", "WHERE qualidade = '1'  AND YEAR(data) = '$ano_orc_selec'  ");
+    $nDenormasseguranca = $pesquisaCtrl->buscarControleNOrc("*", "WHERE normasseguranca = '1'  AND YEAR(data) = '$ano_orc_selec'  ");
+    $nDeapresentacao = $pesquisaCtrl->buscarControleNOrc("*", "WHERE apresentacao = '1'  AND YEAR(data) = '$ano_orc_selec'  ");
+    $nDeenvolvimento = $pesquisaCtrl->buscarControleNOrc("*", "WHERE envolvimento = '1'  AND YEAR(data) = '$ano_orc_selec'  ");
+    $nDeeducacao = $pesquisaCtrl->buscarControleNOrc("*", "WHERE educacao = '1'  AND YEAR(data) = '$ano_orc_selec'  ");
+    $nDeorganizacao = $pesquisaCtrl->buscarControleNOrc("*", "WHERE organizacao = '1'  AND YEAR(data) = '$ano_orc_selec'  ");
+    $nDecompetencia = $pesquisaCtrl->buscarControleNOrc("*", "WHERE competencia = '1'  AND YEAR(data) = '$ano_orc_selec'  ");
+    $nDeorcamento = $pesquisaCtrl->buscarControleNOrc("*", "WHERE orcamento = '1'  AND YEAR(data) = '$ano_orc_selec'  ");
+    $nDeservico = $pesquisaCtrl->buscarControleNOrc("*", "WHERE servico = '1'  AND YEAR(data) = '$ano_orc_selec'  ");
+    $nDesatisfeito = $pesquisaCtrl->buscarControleNOrc("*", "WHERE satisfeito = '1'  AND YEAR(data) = '$ano_orc_selec'  ");
+    //var_dump($nDeConfiabilidade);
     ?>
 
     <div class="col-md-3" >
         <ul class="w3-ul w3-border w3-col l4 m5">
-            <li><span class="w3-badge elfi_cor_fundo_tabela elfi_cor_padrao_texto w3-right"><?= Formatar::moedaBR((count($nDeConfiabilidade) / $totalPesquisasRespondidas) * 100) . "%" ?></span> Confiabilidade </li>
-            <li><span class="w3-badge elfi_cor_fundo_tabela elfi_cor_padrao_texto w3-right"><?= Formatar::moedaBR((count($nDepontualidade) / $totalPesquisasRespondidas) * 100) . "%" ?></span> Pontualidade </li>
-            <li><span class="w3-badge elfi_cor_fundo_tabela elfi_cor_padrao_texto w3-right"><?= Formatar::moedaBR((count($nDedisponibilide) / $totalPesquisasRespondidas) * 100) . "%" ?></span> Disponibilidade </li>
-             <li><span class="w3-badge elfi_cor_fundo_tabela elfi_cor_padrao_texto w3-right"><?= Formatar::moedaBR((count($nDequalidade) / $totalPesquisasRespondidas) * 100) . "%" ?></span> Qualidade </li>
-             <li><span class="w3-badge elfi_cor_fundo_tabela elfi_cor_padrao_texto w3-right"><?= Formatar::moedaBR((count($nDeapresentacao) / $totalPesquisasRespondidas) * 100) . "%" ?></span> Apresentação </li>
-             <li><span class="w3-badge elfi_cor_fundo_tabela elfi_cor_padrao_texto w3-right"><?= Formatar::moedaBR((count($nDeenvolvimento) / $totalPesquisasRespondidas) * 100) . "%" ?></span> Envolvimento </li>
-             <li><span class="w3-badge elfi_cor_fundo_tabela elfi_cor_padrao_texto w3-right"><?= Formatar::moedaBR((count($nDeeducacao) / $totalPesquisasRespondidas) * 100) . "%" ?></span> Educação </li>
-             <li><span class="w3-badge elfi_cor_fundo_tabela elfi_cor_padrao_texto w3-right"><?= Formatar::moedaBR((count($nDeorganizacao) / $totalPesquisasRespondidas) * 100) . "%" ?></span> Organização </li>
-             <li><span class="w3-badge elfi_cor_fundo_tabela elfi_cor_padrao_texto w3-right"><?= Formatar::moedaBR((count($nDecompetencia) / $totalPesquisasRespondidas) * 100) . "%" ?></span> Competencia </li>
-              <li><span class="w3-badge elfi_cor_fundo_tabela elfi_cor_padrao_texto w3-right"><?= Formatar::moedaBR((count($nDeorcamento) / $totalPesquisasRespondidas) * 100) . "%" ?></span> Prazo entrega do Orçamento </li>
-              <li><span class="w3-badge elfi_cor_fundo_tabela elfi_cor_padrao_texto w3-right"><?= Formatar::moedaBR((count($nDeservico) / $totalPesquisasRespondidas) * 100) . "%" ?></span> Prazo entrega do Serviço </li>
-               
-             
-             
-              <li class="elfi_cor_fundo_indice elfi_cor_padrao_texto"><span class="w3-badge elfi_cor_fundo_branco elfi_cor_padrao_texto w3-right"><?= Formatar::moedaBR((count($nDesatisfeito) / $totalPesquisasRespondidas) * 100) . "%" ?></span> Satisfação no Geral</li> 
+            <li><span class="w3-badge elfi_cor_fundo_tabela elfi_cor_padrao_texto w3-right"><?= Formatar::moedaBR((count($nDeConfiabilidade) / ($totalPesquisasRespondidas == 0 ? 1 : $totalPesquisasRespondidas)) * 100) . "%" ?></span> Confiabilidade </li>
+            <li><span class="w3-badge elfi_cor_fundo_tabela elfi_cor_padrao_texto w3-right"><?= Formatar::moedaBR((count($nDepontualidade) / ($totalPesquisasRespondidas == 0 ? 1 : $totalPesquisasRespondidas)) * 100) . "%" ?></span> Pontualidade </li>
+            <li><span class="w3-badge elfi_cor_fundo_tabela elfi_cor_padrao_texto w3-right"><?= Formatar::moedaBR((count($nDedisponibilide) / ($totalPesquisasRespondidas == 0 ? 1 : $totalPesquisasRespondidas)) * 100) . "%" ?></span> Disponibilidade </li>
+            <li><span class="w3-badge elfi_cor_fundo_tabela elfi_cor_padrao_texto w3-right"><?= Formatar::moedaBR((count($nDequalidade) / ($totalPesquisasRespondidas == 0 ? 1 : $totalPesquisasRespondidas)) * 100) . "%" ?></span> Qualidade </li>
+            <li><span class="w3-badge elfi_cor_fundo_tabela elfi_cor_padrao_texto w3-right"><?= Formatar::moedaBR((count($nDeapresentacao) / ($totalPesquisasRespondidas == 0 ? 1 : $totalPesquisasRespondidas)) * 100) . "%" ?></span> Apresentação </li>
+            <li><span class="w3-badge elfi_cor_fundo_tabela elfi_cor_padrao_texto w3-right"><?= Formatar::moedaBR((count($nDeenvolvimento) / ($totalPesquisasRespondidas == 0 ? 1 : $totalPesquisasRespondidas)) * 100) . "%" ?></span> Envolvimento </li>
+            <li><span class="w3-badge elfi_cor_fundo_tabela elfi_cor_padrao_texto w3-right"><?= Formatar::moedaBR((count($nDeeducacao) / ($totalPesquisasRespondidas == 0 ? 1 : $totalPesquisasRespondidas)) * 100) . "%" ?></span> Educação </li>
+            <li><span class="w3-badge elfi_cor_fundo_tabela elfi_cor_padrao_texto w3-right"><?= Formatar::moedaBR((count($nDeorganizacao) / ($totalPesquisasRespondidas == 0 ? 1 : $totalPesquisasRespondidas)) * 100) . "%" ?></span> Organização </li>
+            <li><span class="w3-badge elfi_cor_fundo_tabela elfi_cor_padrao_texto w3-right"><?= Formatar::moedaBR((count($nDecompetencia) / ($totalPesquisasRespondidas == 0 ? 1 : $totalPesquisasRespondidas)) * 100) . "%" ?></span> Competencia </li>
+            <li><span class="w3-badge elfi_cor_fundo_tabela elfi_cor_padrao_texto w3-right"><?= Formatar::moedaBR((count($nDeorcamento) / ($totalPesquisasRespondidas == 0 ? 1 : $totalPesquisasRespondidas)) * 100) . "%" ?></span> Prazo entrega do Orçamento </li>
+            <li><span class="w3-badge elfi_cor_fundo_tabela elfi_cor_padrao_texto w3-right"><?= Formatar::moedaBR((count($nDeservico) / ($totalPesquisasRespondidas == 0 ? 1 : $totalPesquisasRespondidas)) * 100) . "%" ?></span> Prazo entrega do Serviço </li>
+
+
+
+            <li class="elfi_cor_fundo_indice elfi_cor_padrao_texto"><span class="w3-badge elfi_cor_fundo_branco elfi_cor_padrao_texto w3-right"><?= Formatar::moedaBR((count($nDesatisfeito) / ($totalPesquisasRespondidas == 0 ? 1 : $totalPesquisasRespondidas)) * 100) . "%" ?></span> Satisfação no Geral</li> 
         </ul>
     </div>
 
